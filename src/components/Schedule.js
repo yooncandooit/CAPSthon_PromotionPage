1f43a7b32c45e34b323dbe34fdd6c0ab17dcd0e3
Jinsoo Yoon <floreo1242@gmail.com>
2024-07-25 17:13:15 +0900

    일정 추가

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -4,7 +4,12 @@ const Schedule = () => {
   return (
     <section id="schedule-section">
       <h2>CAPSthon 일정</h2>
-      <p>CAPSthon 일정 관련 내용</p>
+      <p>
+        기간: 7/29 ~ 8/30 <br/><br/>
+        - 랜덤 팀 발표: 7/28<br/>
+        - 기획서 제출: 8/7<br/>
+        - 최종 평가(온라인): 8/30
+      </p>
     </section>
   );
 }
